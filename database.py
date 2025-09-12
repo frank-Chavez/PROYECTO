5db49eda9f876d9f888b4f52332fc462aebf15e5
Frank <frankchavez0620@gmail.com>
2025-09-12 17:47:52 -0500

    Eliminar todo el contenido del repo para empezar de cero

--- database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-
-def conection():
-    conn = sqlite3.connect("database.db")  # tu archivo .db
-    conn.row_factory = sqlite3.Row  # para acceder a las columnas por nombre
-    return conn
